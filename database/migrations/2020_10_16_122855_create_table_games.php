9e138e1d96d84d60483182c072311382587c8294
FRANCOIS OLIVIER <olivier.francois2@etu.unistra.fr>
2020-10-16 15:04:14 +0200

    Création de la table game, création de routes avec le controller

---
+++ database/migrations/2020_10_16_122855_create_table_games.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableGames extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('title', 255);
+            $table->string('summary', 255)->nullable();
+            $table->date('released');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\GameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*
+Route::get('/game', function() {
+	return App\Models\Game::all();
+});*/
+
+// Version plus propre
+Route::apiResource('/game', GameController::class);
